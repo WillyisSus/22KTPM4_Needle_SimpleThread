5a62ddc5e5851d7b9a77de6cd817436f4f99df2d
WillyisSus <vovietlong0845927889@gmail.com>
2024-12-25 13:40:54 +0700

    thêm thư viện sequelize core, fix lỗi authentication
    Kiểm tra sự tồn tại của username và email trước khi tạo tài khoản

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -3,6 +3,7 @@ const models = require('../models');
 const bcrypt = require('bcrypt')
 const passport = require('passport');
 const initializePassport = require('../passport-config');
+const {Op} = require('@sequelize/core')
 const { validationResult } = require('express-validator');
 
 initializePassport(passport);
@@ -24,7 +25,19 @@ controller.getSignup = async (req, res) => {
 
     try {
         const hashedPassword = await bcrypt.hash(req.body.Password, 10);
-        models.User.create({
+        const usernameInDB = await models.User.findOne({
+            where: {username: req.body.Username}
+        })
+        if (usernameInDB != null){
+            return res.render("signup", { layout: "logged-out-layout", message: "Username has been used" });
+        }
+        const emailInDB = await models.User.findOne({
+            where: {email: req.body.Email}
+        })
+        if (emailInDB != null){
+            return res.render("signup", { layout: "logged-out-layout", message: "Email has been used" });
+        }
+        await models.User.create({
             username: req.body.Username,
             display_name: req.body.Username,
             email: req.body.Email,
@@ -33,7 +46,7 @@ controller.getSignup = async (req, res) => {
         res.redirect("/auth/login");
     } catch (error) {
         console.log(error);
-        res.redirect("/auth/signup");
+        return res.render("signup", { layout: "logged-out-layout", message: "Something wrong happened" });
     }
 }
 
