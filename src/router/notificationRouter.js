a61f45b3be240135de7c9996357a5ec17ad45574
ibhnim4002 <nguyenlethanhduy2004@gmail.com>
2024-12-26 19:04:07 +0700

    add search func

--- src/router/notificationRouter.js
+++ src/router/notificationRouter.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const controller = require('../controller/notificationController');
-
-router.get("/", controller.showNotif);
+function checkAuthentication(req, res, next){
+    if (req.isAuthenticated()){
+        return next()
+    }
+    res.redirect('/auth/login')
+}
+router.get("/", checkAuthentication, controller.showNotif);
 
 router.post("/mark", controller.markRead);
 router.post("/del", controller.markDel);
